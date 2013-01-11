658a3c77e22b9b5b51b65847d1d208fa2eb655d2
jshaffe <joel.shaffer@cerner.com>
2013-01-11 15:09:39 -0600

    Added updating projects functionality

---
+++ features/step_definitions/project_steps.rb
@@ -0,0 +1,10 @@
+Given /^there is a project called "([^\"]*)"$/ do |name|
+  FactoryGirl.create(:project, :name => name)
+end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).select(:path, :query).compact.join('?')
+  the_test = FactoryGirl.create(:project, :name => page_name)
+  puts current_path
+  current_path.should == '/projects/1'
+end
